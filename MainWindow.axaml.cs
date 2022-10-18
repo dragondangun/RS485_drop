047ef08e4c49639128e67ea6e4df55dfbbb9abb0
dragondangun <dan.sura@mail.ru>
2022-10-18 12:05:34 +0300

    Initial commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace RS485_drop {
+    public partial class MainWindow:Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
